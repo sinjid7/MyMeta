c725c0de7e69d008c95b2396ffedc377134847e7
sinjid7 <wldmstls01@naver.com>
2025-02-20 12:27:55 +0900

    ControllerScriptAdd

---
+++ Assets/Scripts/Entity/BaseController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BaseController : MonoBehaviour
+{
+    protected Rigidbody2D _rigidbody2D;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 Lookdirection { get { return lookDirection; } }
+
+
+    protected virtual void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    
+    protected virtual void Start()
+    {
+
+    }
+
+
+    protected virtual void Update()
+    {
+        HandleAction();
+        Rotate(lookDirection);
+    }
+
+    protected virtual void HandleAction()
+    {
+
+    }
+
+    private void Movement(Vector2 direction)
+    {
+        direction = direction * 5;
+
+        _rigidbody2D.velocity = direction;
+    }
+
+    private void Rotate(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bool isLeft = Mathf.Abs(rotZ) > 90f;
+
+        characterRenderer.flipX = isLeft;
+    }
+}
--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : BaseController
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera m_camera;
+    
+    protected override void Start()
     {
-        
+        base.Start();
+        m_camera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void HandleAction()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("vertical");
+        movementDirection = new Vector2(horizontal, vertical).normalized;
+
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 worldPos = m_camera.ScreenToWorldPoint(mousePosition);
+        lookDirection = (worldPos - (Vector2)transform.position);
+
+        if (lookDirection.magnitude < 0.9f)
+        {
+            lookDirection = Vector2.zero;
+        }
+        else
+        {
+            lookDirection = lookDirection.normalized;
+        }
     }
 }
